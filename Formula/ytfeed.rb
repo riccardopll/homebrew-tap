2e5edcfb6e29e2d50ab3a91d7a3de384de44cdc6
Riccardo Palleschi <riccardo.palleschi@gmail.com>
2026-04-28 02:38:31 +0200

    Fix ytfeed formula for tap syntax

--- Formula/ytfeed.rb
+++ Formula/ytfeed.rb
@@ -3,14 +3,11 @@
 class Ytfeed < Formula
   desc "Scrape YouTube homepage videos into an LLM-friendly format"
   homepage "https://github.com/riccardopll/ytfeed"
+  url "https://github.com/riccardopll/ytfeed/releases/download/0.0.5/ytfeed-0.0.5-arm64-macos.tar.gz"
+  sha256 "1ab6859b49f40a4695e76d41f15fb1603887f43f566c93876171880cef6c5baa"
   license "MIT"
 
   on_macos do
-    on_arm do
-      url "https://github.com/riccardopll/ytfeed/releases/download/0.0.5/ytfeed-0.0.5-arm64-macos.tar.gz"
-      sha256 "1ab6859b49f40a4695e76d41f15fb1603887f43f566c93876171880cef6c5baa"
-    end
-
     on_intel do
       url "https://github.com/riccardopll/ytfeed/releases/download/0.0.5/ytfeed-0.0.5-x86_64-macos.tar.gz"
       sha256 "658c4bddca47f624913f1d9338e5a3c2ce27e318d097299c9f78b20c1e7c346f"
